396ad1b84478036f8d84fc252842f65a1203cd7a
elena <elenastarck1992@mail.ru>
2023-09-04 13:31:30 +0300

    add successful tests for class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+def test_item_init(item1):
+    isinstance(item1.name, str)
+    isinstance(item1.price, float)
+    isinstance(item1.quantity, int)
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 60000
+
+def test_apply_discount(item1):
+    item1.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 10000
+
